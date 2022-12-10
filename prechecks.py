9a594886cf6ecb19c5c4c441252b2b91db7c4f04
phani051 <phani051@gmail.com>
2022-12-10 14:03:28 +0530

    Added User Valdation

--- prechecks.py
+++ prechecks.py
@@ -1,5 +1,6 @@
 import os
 import math
+import pandas as pd
 
 class bcolors:
     HEADER = '\033[95m'
@@ -15,17 +16,27 @@ class bcolors:
 
 # acc_name = input("Account name as per Manage portal: ")
 # acc_datacentre = input("Data Centre: ")
-data_size = input("Data Size as per CoC: ")
+data_size = input("Data Size as per CoC in GB: ")
 file_count = input("File count as per CoC: ")
 # mapping_dir = input("Path of Mapping File: ")
 data_dir = input("Path of Data: ")
 data_dir = data_dir.replace("\\","/")
 data_size = float(data_size)
 
+mappingfile_path = input('Please enter path of mapping file: ')
+accounts_path = input('Please enter path of accounts file: ')
+
+mappingfile_path = mappingfile_path.replace("\\","/")
+accounts_path = accounts_path.replace("\\","/")
+
+mappingfile = pd.read_excel(mappingfile_path)
+accounts = pd.read_excel(accounts_path)
+
+
 
 # Data Validation ======================================
 
-print(" \n****************************** \n\t Data Validation \n ******************************")
+print(" \n****************************** \n\t Data Validation \n******************************")
 
 size = 0
 # get size
@@ -63,4 +74,19 @@ else:
     print("Validation Status: " + bcolors.FAIL + "Fail" + bcolors.ENDC)
 
 
-input(" \n Press enter to exit;")
+
+# User Validation ======================================
+
+print(" \n****************************** \n\t User Validation \n ******************************")
+
+# print('****************************** ','\n Mapping File \n',mappingfile, '\n','****************************** ','\n Accounts \n',accounts[['Name']])
+
+comparission = (pd.merge(mappingfile,accounts, how='outer',indicator='position'))
+missingusers = comparission.loc[comparission['position'] == 'left_only']
+if missingusers.empty:
+    print("Validation Status: " + bcolors.OKGREEN + 'Successful' + bcolors.ENDC)
+else:
+    print('****************************** ',"\nValidation Status: " + bcolors.FAIL + 'Failed' + + bcolors.ENDC  ,'\n Missing Users \n',missingusers[['Name']])
+
+
+input("Press enter to exit;")
